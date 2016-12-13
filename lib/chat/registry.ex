5cbb49d6342b5f40160943e22ed20b488b1523c5
Weijun Yin <gw9812@gmail.com>
2016-12-13 17:26:20 +0800

    Registry monitors the registred pid

--- lib/chat/registry.ex
+++ lib/chat/registry.ex
@@ -44,7 +44,11 @@ defmodule Chat.Registry do
   def handle_call({:register_name, room_name, pid}, _from, state) do
   	# registering a name is just a matter of putting it in our map.
   	case Map.get(state, room_name) do
-  	  nil -> {:reply, :yes, Map.put(state, room_name, pid)}
+  	  nil -> 
+        # When a new process is registered, we start monitoring it
+        Process.monitor(pid)
+  	  	{:reply, :yes, Map.put(state, room_name, pid)}
+
   	  _ -> {:reply, :no, state}
   	end
   end
@@ -52,6 +56,21 @@ defmodule Chat.Registry do
   def handle_cast({:unregister_name, room_name}, state) do
   	# and unregistering is as simple as deleting an entry from our map.
   	{:noreply, Map.delete(state, room_name)}
+  end
+
+  def handle_info({:DOWN, _, :process, pid, _}, state) do
+  	# When a monitored process dies, we will receive a 
+  	# :DOWN message that we can use to remove the dead pid from our registry.
+  	{:noreply, remove_pid(state, pid)}
+  end
+
+  defp remove_pid(state, pid_to_remove) do
+  	# remove = fn {_key, pid} -> pid != pid_to_remove end
+  	# Enum.filter(state, remove) |> Enum.into(%{})
+
+  	for {key, pid} <- state, pid != pid_to_remove, into: %{} do
+  	  {key, pid}
+  	end
   end
 
 end
